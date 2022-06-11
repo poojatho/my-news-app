bbda80d11ee3a40fa77dfad3fb220a619ca2015e
747930 <sarbeswar1999@gmail.com>
2022-06-11 16:50:38 +0530

    k

--- newsFrontend/src/app/pages/login/login.component.ts
+++ newsFrontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService, private snack:MatSnackBar) { }
 
-  ngOnInit(): void {
+  public user={
+    username: '',
+    password: '',
+  
+  };
+
+  ngOnInit(): void {}
+
+  formSubmit(){
+    console.log(this.user);
+    if(this.user.username=='' || this.user.username == null){
+      //alert('User is required');
+      this.snack.open("Username is required",'',{
+        duration:3000,
+        // verticalPosition:'top',
+        // horizontalPosition:'right',
+      })
+      return;
+    }
+
+    //addUser: userservice
+
+    this.userService.signin(this.user).subscribe(
+      (data)=>{
+        //success
+        console.log(data);
+        //alert('success');
+        Swal.fire('Successfully done!!', 'User is signed in','success')
+      },
+      (error)=>{
+        console.log(error);
+        // alert('something went wrong');
+        this.snack.open('Something went wrong','',{
+          duration:3000,
+        })
+      }
+      
+    )
   }
 
 }
+
--- newsFrontend/src/app/services/user.service.ts
+++ newsFrontend/src/app/services/user.service.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 import baseUrl from './helper';
 
-
+interface IToken {
+  token:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  token: string = ''; 
+  user: BehaviorSubject<any> = new BehaviorSubject(null);
+ 
 
   constructor(    private http:HttpClient  ) { }
 
@@ -16,4 +23,14 @@ export class UserService {
     return this.http.post(`${baseUrl}/user/` , user);
 
   }
+  //signin
+  signin(user:any){
+    return this.http.post(`${baseUrl}/generate-token` , user).pipe(tap((data:any)=> {this.token=(data.token)}))
+
+  }
+  //
+  // getLoggedinUser(token,userId){
+  //   return this.http.post(`${baseUrl}/generate-token` , user)
+  // }
+
 }
